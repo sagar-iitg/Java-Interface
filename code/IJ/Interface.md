e3f955cf3e3b61530400bd67549755095e836507
sagar <sagar-iitg>
2023-04-22 19:58:13 +0530

    added Interface.md

---
+++ code/IJ/Interface.md
@@ -0,0 +1,22 @@
+1.Why do we need Interface, when we already have abstract classes?
+
+
+
+1. Why Interface?
+2. Diamond Problem ?
+3. Multiple Inheritance. ?
+4.  Default Method. ?
+5.  Scenario based Interview questions. ?
+
+
+
+### *facts*
+
+
+
+
+
+
+
+
+
---
+++ code/IJ/src/com/sk/interfcae/Test.java
@@ -0,0 +1,30 @@
+package com.sk.interfcae;
+
+
+abstract class Test1{
+
+    public abstract void m1();
+
+
+}
+
+
+abstract class Test2{
+
+    public abstract void m1();
+
+}
+
+
+public class Test extends Test1,Test2{
+
+
+    public static void main(String[] args) {
+        new Test().m1();
+    }
+
+    @Override
+    public void m1() {
+        System.out.println("Test1 m1 method");
+    }
+}
